b0e7b491a79dbefe27185281d7a70ad4eff82f04
saiful <niloysaif9@gmail.com>
2022-11-18 11:02:49 +0600

    Finally fetch system solved

--- shop/app.js
+++ shop/app.js
@@ -1,57 +1,102 @@
 
 
-//Get elements
+//without axios fetch system
+//Get all product by Fetch
 
+// const getAllProduct = async () => {
+
+//     try {
+
+//        await fetch('http://localhost:5050/api/v1/product')
+
+//         .then(res=> {
+//            return res.json()
+//         })
+//         .then(res => console.log(res))
+//         .catch(error => console.log(error.message))
+
+        
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
+// getAllProduct()
+
+
+//Get form Data 
+
+// product_form.onsubmit = async (e) => {
+//     e.preventDefault()
+    
+//     const formData = new FormData(e.target);
+//     const data = Object.fromEntries(formData.entries());
+
+//     try {
+
+//         await fetch('http://localhost:5050/api/v1/product', {
+//             method : "POST",
+//             body : JSON.stringify(data),
+//             headers : {
+//                 'Content-Type' : 'application/json'
+//             }
+//         })
+        
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+    
+    
+    
+// }
+
+
+//Using axios fetch system
+
+
+
+//Get elements
 const product_form = document.getElementById('product_form');
 
-// /**
-//  * Get All product
-//  */
+
+//Get all product by Fetch
 
 const getAllProduct = async () => {
 
-
     try {
 
-       await axios.get('localhost:5050/api/v1/product')
-       .then( res => {
-        console.log(res.data);
+       await axios.get('http://localhost:5050/api/v1/product')
+       .then(res => {
+            console.log(res.data);
        })
-       .catch( error => console.log(error.message))
-     
+       .catch(error => console.log(error.message))
 
-    } catch (error) {
         
+    } catch (error) {
         console.log(error.message);
-
     }
-
-
 }
 
 getAllProduct()
 
-//Get form data
 
-product_form.onsubmit = async (e)=> {
-    e.preventDefault();
+//Get form Data 
+
+product_form.onsubmit = async (e) => {
+    e.preventDefault()
     
-    const formData = new FormData(e.target)
-    // const data = Object.fromEntries(formData.entries());
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
 
-    console.log(formData);
-
-    
     try {
 
-       await axios.post('localhost:5050/api/v1/product', formData)
-       .then( res => {
-        console.log(res.data);
-       })
-       .catch( error => console.log(error.message))
+      await  axios.post('http://localhost:5050/api/v1/product', data)
+
+
         
     } catch (error) {
         console.log(error.message);
     }
-   
+    
+    
+    
 }
