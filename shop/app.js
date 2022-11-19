cdddbce09094e80a5c4807162770cd8cef4e8545
saiful <niloysaif9@gmail.com>
2022-11-19 10:37:36 +0600

    API Project done

--- shop/app.js
+++ shop/app.js
@@ -56,6 +56,7 @@
 
 //Get elements
 const product_form = document.getElementById('product_form');
+const product_list = document.getElementById('product_list');
 
 
 //Get all product by Fetch
@@ -65,8 +66,27 @@ const getAllProduct = async () => {
     try {
 
        await axios.get('http://localhost:5050/api/v1/product')
+
        .then(res => {
-            console.log(res.data);
+            
+            let list = '';
+
+        res.data.forEach( ( item, index ) => {
+            list += `<tr class="align-middle">
+                        <td>${index + 1}</td>
+                        <td>${item.name}</td>
+                        <td>${item.regularPrice}</td>
+                        <td>${item.sellPrice}</td>
+                        <td>${item.stock}</td>
+                        <td><img style="width: 40px; height: 40px; object-fit: cover" src="http://192.168.0.133:5050/images/product/${item.photo}"></td>
+                        <td>${item.category}</td>
+                        <td>${item.tag}</td>
+                    </tr>`
+        });
+
+        product_list.innerHTML = list
+
+
        })
        .catch(error => console.log(error.message))
 
@@ -85,15 +105,20 @@ product_form.onsubmit = async (e) => {
     e.preventDefault()
     
     const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
+    // const data = Object.fromEntries(formData.entries());
 
     try {
 
-      await  axios.post('http://localhost:5050/api/v1/product', data)
-
-
+      await  axios.post('http://localhost:5050/api/v1/product', formData)
+            //  .then(res => {
+            //     console.log(res.data);
+            //  })
+            
+             .catch(error => console.log(error.message))
+             
         
     } catch (error) {
+
         console.log(error.message);
     }
     
